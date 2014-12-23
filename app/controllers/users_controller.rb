f5924095ea1e865152ccf04ed92ded1c0730256a
Steve Rogers <srogers.design@gmail.com>
2014-12-23 07:39:15 -0500

    initial commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+  # def index
+  # end
+
+  # def show
+  # # end
+  # #
+  # def new
+  # end
+  #
+  # def create
+  # end
+  #
+  # def destroy
+  #   redirect_to '/'
+  # end
+  # # private
+  #
+  # # def television_show_params
+  #   params.require(:television_show).permit(:title, :network, :years, :synopsis)
+  # end
+end
